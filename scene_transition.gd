6f5782cde69759f423d22c519dff97c80d98c306
1201354 (Rodrigo Soares) <121984256+RodrigoSoares1201354@users.noreply.github.com>
2024-07-20 16:08:23 +0100

    small fix

--- scene_transition.gd
+++ scene_transition.gd
@@ -3,5 +3,5 @@ extends CanvasLayer
 func change_scene(target: StringName, cur_scene: Node) -> void:
 	$AnimationPlayer.play("dissolve")
 	await $AnimationPlayer.animation_finished
-	Utilities.switch_scene(target,cur_scene)
+	Utilities.switch_scene_with_clean_up(target,cur_scene)
 	$AnimationPlayer.play_backwards("dissolve")
