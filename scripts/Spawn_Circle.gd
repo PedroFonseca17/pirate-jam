22490fe565fd708ad22079e8fdbc3fa476227f4a
1201354 (Rodrigo Soares) <121984256+RodrigoSoares1201354@users.noreply.github.com>
2024-07-27 16:23:26 +0100

    logistics to make a spawn circle

---
+++ scripts/Spawn_Circle.gd
@@ -0,0 +1,27 @@
+extends Node
+
+@onready var sprite_2d = $Sprite2D
+
+var entered_body: bool = false
+
+func _ready():
+	pass
+
+func _process(delta):
+	if entered_body and Input.is_action_just_pressed("interact"):
+		flip()
+
+
+func flip():
+	sprite_2d.texture = load("res://assets/tiles/floral-walls.png")
+
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("Player"):
+		entered_body = true
+	
+
+
+func _on_area_2d_body_exited(body):
+	if body.is_in_group("Player"):
+		entered_body = false
